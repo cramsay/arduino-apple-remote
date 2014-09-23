52829c4c65338f548c3134db113a6c2674da347c
Craig Ramsay <cramsay01@gmail.com>
2014-09-23 14:15:19 +0100

    Refactored to use default lib

--- apple_remote.ino
+++ apple_remote.ino
@@ -1,4 +1,5 @@
-#include "IRremote.h"
+#include <IRremote.h>
+#include <IRremoteInt.h>
 IRsend irsend;
 
 //Cmd codes
@@ -19,32 +20,58 @@ IRsend irsend;
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-char cmd_char;
-  if ( -1 != (cmd_char=Serial.read()) ) {
-	switch(cmd_char){
-		case CMD_KEY_UP:
-			send_cmd(CMD_BYT_UP);break;
-		case CMD_KEY_DOWN:
-			send_cmd(CMD_BYT_DOWN);break;
-		case CMD_KEY_LEFT:
-			send_cmd(CMD_BYT_LEFT);break;
-		case CMD_KEY_RIGHT:
-			send_cmd(CMD_BYT_RIGHT);break;
-		case CMD_KEY_CENTRE:
-			send_cmd(CMD_BYT_CENTRE);break;
-		case CMD_KEY_MENU:
-			send_cmd(CMD_BYT_MENU);break;
-		default: break;
-	}
-  }
-} 
+    char cmd_char;
+    if ( -1 != (cmd_char=Serial.read()) ) {
+        switch(cmd_char) {
+        case CMD_KEY_UP:
+            send_cmd(CMD_BYT_UP);
+            break;
+        case CMD_KEY_DOWN:
+            send_cmd(CMD_BYT_DOWN);
+            break;
+        case CMD_KEY_LEFT:
+            send_cmd(CMD_BYT_LEFT);
+            break;
+        case CMD_KEY_RIGHT:
+            send_cmd(CMD_BYT_RIGHT);
+            break;
+        case CMD_KEY_CENTRE:
+            send_cmd(CMD_BYT_CENTRE);
+            break;
+        case CMD_KEY_MENU:
+            send_cmd(CMD_BYT_MENU);
+            break;
+        default:
+            break;
+        }
+    }
+}
 
 void send_cmd(byte cmd_code)
 {
     byte data[] = {0xEE,0x87,cmd_code,0x59};
-    irsend.sendAPP(data,4);
+
+    irsend.enableIROut(38);
+    irsend.mark(NEC_HDR_MARK);
+    irsend.space(NEC_HDR_SPACE);
+    for (int i = 0; i < sizeof(data)/sizeof(byte); i++) {
+        for(int j=0; j<8; j++) {
+            if (data[i] & 1) {
+                irsend.mark(NEC_BIT_MARK);
+                irsend.space(NEC_ONE_SPACE);
+            }
+            else {
+                irsend.mark(NEC_BIT_MARK);
+                irsend.space(NEC_ZERO_SPACE);
+            }
+            data[i] >>= 1;
+        }
+    }
+    irsend.mark(NEC_BIT_MARK);
+    irsend.space(0);
 }
+
